37675ef9aed5e6345c99fde0d7b42bf764b252ac
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-06-05 21:32:51 -0300

    create about page

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def about(request):
+    return HttpResponse("Hello World!")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('about/', about)
 ]
